063794bcd3fc3655e84158b81cd205fb6f80a80c
N4-RC <ktc25a31e0007@edu.kyoto-tech.ac.jp>
2025-11-29 16:17:45 +0900

    style: update layout and fix CSS class usage in page component

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,14 @@ import Styles from "./page.module.css"
 
 export default function Home() {
   return(
+    <section ClassName={styles.top}>
     <div>
       <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
       <p className={styles.description}>
         私たちは市場をリードしているグローバルっテックカンパニーです。
       </p>
     </div>
+    <img ClassName={styles.bgimg} src="/img-mv.jpg" alt="" />
+    </section>
   );
 }
